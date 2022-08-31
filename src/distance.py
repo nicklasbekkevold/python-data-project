04385d2f54fba10dfbd01b1428b44aede7895543
44287251+nicklasbekkevold@users.noreply.github.com <nicklasbekkevold@gmail.com>
2022-08-31 09:37:18 +0200

    Refactor variables for more consistent notation

--- src/distance.py
+++ src/distance.py
@@ -1,13 +1,13 @@
 import math
 
 
-def manhattan_distance(X, Y):
-    return sum([abs(x - y) for x, y in zip(X, Y)])
+def manhattan_distance(x, y):
+    return sum([abs(x_i - y_i) for x_i, y_i in zip(x, y)])
 
 
-def euclidean_distance(X, Y):
-    return math.sqrt(sum([(x - y) ** 2 for x, y in zip(X, Y)]))
+def euclidean_distance(x, y):
+    return math.sqrt(sum([(x_i - y_i) ** 2 for x_i, y_i in zip(x, y)]))
 
 
-def minkowski_distance(X, Y, p=2):
-    return sum([abs(x - y) ** p for x, y in zip(X, Y)]) ** (1 / p)
+def minkowski_distance(x, y, p=2):
+    return sum([abs(x_i - y_i) ** p for x_i, y_i in zip(x, y)]) ** (1 / p)
--- tests/test_distance.py
+++ tests/test_distance.py
@@ -4,34 +4,34 @@ from .context import distance
 
 
 @pytest.fixture
-def X():
+def x():
     return (0, 0)
 
 
 @pytest.fixture
-def Y():
+def y():
     return (3, 4)
 
 
-def test_manhattan_distance(X, Y):
-    manhattan = distance.manhattan_distance(X, Y)
-    assert manhattan == 7
+def test_manhattan_distance(x, y):
+    d_manhattan = distance.manhattan_distance(x, y)
+    assert d_manhattan == 7
 
 
-def test_euclidean_distance(X, Y):
-    euclidean = distance.euclidean_distance(X, Y)
-    assert euclidean == 5
+def test_euclidean_distance(x, y):
+    d_euclidean = distance.euclidean_distance(x, y)
+    assert d_euclidean == 5
 
 
-def test_minkowski_distance_manhattan_case(X, Y):
-    manhattan = distance.manhattan_distance(X, Y)
-    minkowski = distance.minkowski_distance(X, Y, p=1)
+def test_minkowski_distance_manhattan_case(x, y):
+    d_manhattan = distance.manhattan_distance(x, y)
+    d_minkowski = distance.minkowski_distance(x, y, p=1)
 
-    assert minkowski == manhattan
+    assert d_minkowski == d_manhattan
 
 
-def test_minkowski_distance_euclidean_case(X, Y):
-    euclidean = distance.euclidean_distance(X, Y)
-    minkowski = distance.minkowski_distance(X, Y, p=2)
+def test_minkowski_distance_euclidean_case(x, y):
+    d_euclidean = distance.euclidean_distance(x, y)
+    d_minkowski = distance.minkowski_distance(x, y, p=2)
 
-    assert minkowski == euclidean
+    assert d_minkowski == d_euclidean
